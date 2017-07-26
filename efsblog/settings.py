2928918d2d9d7267b7aeedb790966158e54d15a9
svishnubhotla <svishnubhotla@unomaha.edu>
2017-07-25 21:16:00 -0500

    IP3 First commit

--- efsblog/settings.py
+++ efsblog/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,12 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '4+c4)dj^j^-t2&1rn#j!qct)mnfsr_1+5wbf)(dw^a9nkm4_6j'
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -80,15 +78,8 @@ WSGI_APPLICATION = 'efsblog.wsgi.application'
 
 DATABASES = {
     'default': {
-       'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbud55e772c0v5',
-        'USER': 'rrptdczebolovk',
-        'PASSWORD': '3ebd1b7a5da7ef62d5d1bc415fd314731680a174dc329ff3941cbd9db1578433',
-        'HOST': 'ec2-107-20-255-96.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -127,30 +118,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.utils import timezone
 from .models import *
 from django.shortcuts import render, get_object_or_404
@@ -8,6 +5,10 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from .forms import *
 from django.db.models import Sum
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CustomerSerializer
 
 
 
@@ -98,24 +99,10 @@ def stock_delete(request, pk):
    return render(request, 'portfolio/stock_list.html', {'stocks': stocks})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@login_required
+def investment_list(request):
+   investments = Investment.objects.filter(acquired_date__lte=timezone.now())
+   return render(request, 'portfolio/investment_list.html', {'investments': investments})
 
 @login_required
 def investment_new(request):
@@ -133,12 +120,11 @@ def investment_new(request):
        # print("Else")
    return render(request, 'portfolio/investment_new.html', {'form': form})
 
-
 @login_required
 def investment_edit(request, pk):
    investment = get_object_or_404(Investment, pk=pk)
    if request.method == "POST":
-       form = InvestmentForm(request.POST, instance= investment)
+       form = InvestmentForm(request.POST, instance=investment)
        if form.is_valid():
            investment = form.save()
            # investment.customer = investment.id
@@ -151,32 +137,12 @@ def investment_edit(request, pk):
        form = InvestmentForm(instance=investment)
    return render(request, 'portfolio/investment_edit.html', {'form': form})
 
-
 @login_required
 def investment_delete(request, pk):
-   investment = get_object_or_404(Investment, pk=pk)
+   investment = get_object_or_404(Stock, pk=pk)
    investment.delete()
    investments = Investment.objects.filter(acquired_date__lte=timezone.now())
    return render(request, 'portfolio/investment_list.html', {'investments': investments})
-
-
-
-
-
-
-
-
-
-
-
-
-
-@login_required
-def investment_list(request):
-   investments = Investment.objects.filter(acquired_date__lte=timezone.now())
-   return render(request, 'portfolio/investment_list.html', {'investments': investments})
-
-
 
 @login_required
 def portfolio(request,pk):
@@ -184,27 +150,21 @@ def portfolio(request,pk):
    customers = Customer.objects.filter(created_date__lte=timezone.now())
    investments =Investment.objects.filter(customer=pk)
    stocks = Stock.objects.filter(customer=pk)
+   sum_recent_value = Investment.objects.filter(customer=pk).aggregate(Sum('recent_value'))
    sum_acquired_value = Investment.objects.filter(customer=pk).aggregate(Sum('acquired_value'))
 
 
    return render(request, 'portfolio/portfolio.html', {'customers': customers, 'investments': investments,
                                                       'stocks': stocks,
+                                                      'sum_recent_value': sum_recent_value,
                                                       'sum_acquired_value': sum_acquired_value,})
 
 
-@login_required
-def stock_edit(request, pk):
-   stock = get_object_or_404(Stock, pk=pk)
-   if request.method == "POST":
-       form = StockForm(request.POST, instance=stock)
-       if form.is_valid():
-           stock = form.save()
-           # stock.customer = stock.id
-           stock.updated_date = timezone.now()
-           stock.save()
-           stocks = Stock.objects.filter(purchase_date__lte=timezone.now())
-           return render(request, 'portfolio/stock_list.html', {'stocks': stocks})
-   else:
-       # print("else")
-       form = StockForm(instance=stock)
-   return render(request, 'portfolio/stock_edit.html', {'form': form})
+# List at the end of the views.py
+# Lists all customers
+class CustomerList(APIView):
+
+    def get(self,request):
+        customers_json = Customer.objects.all()
+        serializer = CustomerSerializer(customers_json, many=True)
+        return Response(serializer.data)
